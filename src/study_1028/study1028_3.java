4192b4096030e8a596ca38517d4e019a258b71c4
scoooly0410 <wofhddl0550@gmail.com>
2025-10-28 19:18:01 +0900

    Changes

--- src/study_1028/study1028_3.java
+++ src/study_1028/study1028_3.java
@@ -6,7 +6,21 @@ public class study1028_3 {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("점수 입력 : ");
+        int score = sc.nextInt();
 
+        if (score > 100 || score < 0)
+            System.out.println("점수가 아닙니다.");
+         else if (score >= 90)
+            System.out.println("학점은 A 입니다.");
+         else if (score >= 80)
+            System.out.println("학점은 B 입니다.");
+         else if (score >= 70)
+            System.out.println("학점은 C 입니다.");
+         else if (score >= 60)
+            System.out.println("학점은 D 입니다.");
+         else
+            System.out.println("학점은 F 입니다.");
 
     }
 }
