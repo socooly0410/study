2c31a0c7e77581387b042e0c882759c7dda1119b
scoooly0410 <wofhddl0550@gmail.com>
2025-10-29 20:02:49 +0900

    Changes

--- src/study_1029/study1029_arr4.java
+++ src/study_1029/study1029_arr4.java
@@ -3,11 +3,21 @@ package study_1029;
 public class study1029_arr4 {
     public static void main(String[] args) {
 
-        int[][] intarr = new int[3][2];
+        int[][] intarr = new int[5][5];
+        int num = 1;
 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                intarr[i][j] = num;
+                num++;
+            }
+        }
 
-
-
-
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%2d ", intarr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
